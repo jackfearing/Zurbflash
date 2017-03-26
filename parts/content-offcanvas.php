babe9a2ddb354d6dab1ec831db35bf0388b22d29
Jack Fearing <fear@cox.net>
2017-03-25 19:06:42 -0700

    Custom ACF Functions
    Added custom ACF functions which allow the user to choose the different options to the off-canvas menu

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -1,3 +1,3 @@
-<div class="off-canvas jack position-<?php echo get_field('off_canvas_menu_position','options');?>" id="off-canvas" data-off-canvas>
+<div class="off-canvas jack position-<?php echo get_field('off_canvas_menu_position','options'); ?>" id="off-canvas" data-off-canvas>
 	<?php joints_off_canvas_nav(); ?>
 </div>

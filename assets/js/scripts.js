a205079642f1ba48c656cedf992d4377f37e62b5
Jack Fearing <fear@cox.net>
2017-04-15 13:16:00 -0700

    Processed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5524,6 +5524,29 @@ if(!$("html").hasClass("touch")){
 
 })(window, jQuery);
 
+// Using Jquery To Add A Dynamic “Back To Top” Floating Button With Smooth Scroll
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
+
+
+
+
+
 /*
 (function(){
 
